979f2e1b2ae0c9abcd6bfa9f2d615d35b208d0c8
Jefferson <jefferson.boldrin@gmail.com>
2019-05-25 01:44:44 -0300

    sets up google analytics page view tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
+
+import { AnalyticsService } from './shared/services/analytics.service';
+
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  constructor (
+    private readonly analyticsService: AnalyticsService,
+    private readonly angularticsGoogleService: Angulartics2GoogleAnalytics,
+    // private readonly router: Router,
+  ) { }
+
+  ngOnInit() {
+    this.analyticsService.init();
+
+    // this.router.events.subscribe(event => {
+    //   if (event instanceof NavigationEnd) {
+    //     (window as any).ga('set', 'page', event.urlAfterRedirects);
+    //     (window as any).ga('send', 'pageview');
+    //   }
+    // });
+
+    this.angularticsGoogleService.startTracking();
+  }
+
+ }
